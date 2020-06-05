c3ab8e76ae96f3176dd3110a3432d87d5f8f7a93
jialang <jianwen.lang@gmail.com>
2020-06-04 22:53:12 -0700

    add cookie test new tab

--- tests/acceptcookie_test.js
+++ tests/acceptcookie_test.js
@@ -1,9 +1,16 @@
 Feature('acceptcookie');
 const CONFIG = require('../config/my.config.js').endpoint;
 
-Scenario('Accept Cookie', I => {
+Scenario('Accept Cookie', async (I) => {
     I.amOnPage(CONFIG.basicurl);
+    I.see('Privacy & Cookies:');
+    I.seeElement('.widget_eu_cookie_law_widget');
     I.click('//input[@value="Close and accept"]')
     I.refreshPage();
-    I.dontSee('Privacy & Cookies:')
-}).tag('@prepare').tag('setup');
+    I.dontSee('Privacy & Cookies:');
+
+    // cookie should not be seen again in more tabs
+    I.openNewTab();
+    I.amOnPage(CONFIG.basicurl);
+    I.dontSee('Privacy & Cookies:');
+}).tag('@smoke')
