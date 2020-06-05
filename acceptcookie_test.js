5692ee11c27f73f0b3632e1f317b3dde25beca32
jialang <jianwen.lang@gmail.com>
2020-06-04 21:14:30 -0700

    Delete acceptcookie_test.js

--- acceptcookie_test.js
+++
@@ -1,8 +0,0 @@
-Feature('acceptcookie');
-
-Scenario('Accept Cookie', I => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/');
-    I.click('//input[@value="Close and accept"]')
-    I.refreshPage();
-    I.dontSee('Privacy & Cookies:')
-}).tag('@prepare').tag('setup');
